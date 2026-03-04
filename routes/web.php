58dbd1f8031965a03b638e79cdb6349eaafe4c57
Greyari <159263690+Greyari@users.noreply.github.com>
2026-03-04 10:09:42 +0700

    update route kerna ada bug 2

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes();
-
 Route::livewire('/', 'home')->name('home');
 Route::livewire('/products', 'product-index')->name('products');
 Route::livewire('/products/liga/{ligaId}', 'product-liga')->name('products.liga');
