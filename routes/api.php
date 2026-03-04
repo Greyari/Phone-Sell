56c002e1da456a004a3648bf9ffc420ddadae6de
Greyari <159263690+Greyari@users.noreply.github.com>
2026-03-04 10:01:11 +0700

    update route kerna ada bug

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\UserController;
+
+Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'index']);
